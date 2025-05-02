cddb114a8cf2e701783f68e975742ad826f35441
jhors <jhonnijhorsf1@gmail.com>
2025-05-02 09:37:27 -0400

    Agregando ruta /api/dolar

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send('¡Hola, mundo!');
+  res.send('¡Hola, mundo!');
+});
+
+// Ruta nueva para mostrar el precio del dólar
+app.get('/api/dolar', (req, res) => {
+  res.json({
+    pais: 'Bolivia',
+    compra: 6.86,
+    venta: 6.96,
+    fuente: 'Banco Central de Bolivia'
+  });
 });
 
 app.listen(port, () => {
-    console.log(`Servidor corriendo en http://localhost:${port}`);
+  console.log(`Servidor corriendo en http://localhost:${port}`);
 });
